2020fcd1db179c59e12c37f9a749901985cb78e0
Krishna <kr7pinku@gmail.com>
2026-09-10 15:59:51 +0530

    Update UI and styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <div className="bg animated">
-      <h1 style={{marginBottom : "20px"}}>Welcome to StudySync AI</h1>
-      <br />
-      <h5>Designed for Better Learning</h5>
+      <div className="hero-text">
+        <h1>Welcome to StudySync AI</h1>
+        <h5>Designed for Better Learning</h5>
+      </div>
     </div>
   );
 }
